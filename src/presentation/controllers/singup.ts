1f3ed8c3b6c24095999849d26f7eecf423640b75
Luan Andrade Ludra <luanandradeti10@gmail.com>
2024-06-08 20:51:34 -0300

    feat: ensure singUpController return an error if no name is provided

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -2,7 +2,8 @@ export class SignUpController {
   handle(httpRequest: object): object {
     if (httpRequest)
       return {
-        statusCode: 400
+        statusCode: 400,
+        body: new Error('Missing param: name')
       }
   }
 }
