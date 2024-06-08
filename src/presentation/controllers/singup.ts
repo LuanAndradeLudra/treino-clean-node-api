93f8164e8227568ec5982ed2632a4a8912f9b5ef
Luan Andrade Ludra <luanandradeti10@gmail.com>
2024-06-08 20:57:45 -0300

    feat: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -1,9 +1,16 @@
 export class SignUpController {
   handle(httpRequest: object): object {
-    if (httpRequest)
+    if (!httpRequest['name']) {
       return {
         statusCode: 400,
         body: new Error('Missing param: name')
       }
+    }
+    if (!httpRequest['email']) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: email')
+      }
+    }
   }
 }
