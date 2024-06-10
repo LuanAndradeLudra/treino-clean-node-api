f46a225e97d7732dcef9436868739a24af822440
Luan Andrade Ludra <luanandradeti10@gmail.com>
2024-06-10 13:25:00 -0300

    feat: ensure SingUpController calls AddAccount with correct values

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -2,29 +2,39 @@ import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest, serverError } from '../helpers/http-helper'
 import { Controller, EmailValidator, httpRequest, httpResponse } from '../protocols'
 import { InvalidParamError } from '../errors/invalid-param-error'
+import { AddAccount } from '../../domain/usecases/add-account'
 
 export class SignUpController implements Controller {
   private readonly validateEmail: EmailValidator
+  private readonly addAccount: AddAccount
 
-  constructor(validateEmail: EmailValidator) {
+  constructor(validateEmail: EmailValidator, addAccount: AddAccount) {
     this.validateEmail = validateEmail
+    this.addAccount = addAccount
   }
 
   handle(httpRequest: httpRequest): httpResponse {
     try {
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredFields) {
-        if (!httpRequest.body[field]) {
+        if (!httpRequest.body![field]) {
           return badRequest(new MissingParamError(field))
         }
       }
 
-      if (httpRequest.body['password'] !== httpRequest.body['passwordConfirmation']) {
+      if (httpRequest.body!['password'] !== httpRequest.body!['passwordConfirmation']) {
         return badRequest(new InvalidParamError('passwordConfirmation'))
       }
 
-      const isValid = this.validateEmail.isValid(httpRequest.body['email'])
+      const isValid = this.validateEmail.isValid(httpRequest.body!['email'])
+
       if (!isValid) return badRequest(new InvalidParamError('email'))
+
+      this.addAccount.add({
+        name: httpRequest.body!['name'],
+        email: httpRequest.body!['email'],
+        password: httpRequest.body!['password']
+      })
     } catch (_) {
       return serverError()
     }
