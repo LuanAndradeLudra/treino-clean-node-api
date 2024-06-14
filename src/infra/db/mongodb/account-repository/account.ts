5cd6ff9651e9dd76608f2cf6f7db6a4c1208211f
Luan Andrade <luanandradeti10@gmail.com>
2024-06-14 20:22:07 -0300

    feat: add mongodb helper and AccountMongoRepository

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,9 @@
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { AccountModel } from '../../../../domain/models/account'
+import { AddAccountModel } from '../../../../domain/usecases/add-account'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  add(accountData: AddAccountModel): Promise<AccountModel> {
+    if (accountData) return null
+  }
+}
