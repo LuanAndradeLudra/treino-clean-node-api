9ec027848577aef24441303ab9ba2f13d888499e
Luan Andrade <luanandradeti10@gmail.com>
2024-06-14 21:44:42 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,9 +1,21 @@
 import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
 import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
+import { MongoHelper } from '../helpers/mongo-helper'
 
 export class AccountMongoRepository implements AddAccountRepository {
-  add(accountData: AddAccountModel): Promise<AccountModel> {
-    if (accountData) return null
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const result = await accountCollection.insertOne(accountData)
+    if (result.acknowledged) {
+      const account = await accountCollection.findOne({ _id: result.insertedId })
+      const accountModel: AccountModel = {
+        id: account._id.toString(),
+        name: account.name,
+        email: account.email,
+        password: account.password
+      }
+      return accountModel
+    } else return null
   }
 }
