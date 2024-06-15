a8e785c9a7a6b59348fa3374535f97fd6d5e84f5
Luan Andrade <luanandradeti10@gmail.com>
2024-06-15 19:16:19 -0300

    feat: connect to mongodb before app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,14 @@
-import app from './config/app'
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
+import env from './config/env'
 
-// eslint-disable-next-line no-console
-app.listen(3000, () => console.log('Server running at http://localhost:3000'))
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    // eslint-disable-next-line no-console
+    console.log(`Database connected at: ${env.mongoUrl}`)
+    const app = (await import('./config/app')).default
+
+    // eslint-disable-next-line no-console
+    app.listen(env.port, () => console.log('Server running at http://localhost:3000'))
+  })
+  // eslint-disable-next-line no-console
+  .catch(console.error)
