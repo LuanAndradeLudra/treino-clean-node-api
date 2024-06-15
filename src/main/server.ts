4a310667925821bf2979462d66de62a4e051de48
Luan Andrade <luanandradeti10@gmail.com>
2024-06-15 14:15:29 -0300

    feat: ensure request parse body as json

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,4 @@
-import express from 'express'
-
-const app = express()
+import app from './config/app'
 
 // eslint-disable-next-line no-console
 app.listen(3000, () => console.log('Server running at http://localhost:3000'))
