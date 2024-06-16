9e87cd9afcd9c72a0752c89dcebe4e8d27699b74
Luan Andrade <luanandradeti10@gmail.com>
2024-06-15 22:36:24 -0300

    test: ensure LogControllerDecorator calls controller with correct value

---
+++ src/main/decorators/log.spec.ts
@@ -0,0 +1,32 @@
+import { Controller, httpRequest, httpResponse } from '../../presentation/protocols'
+import { LogControllerDecorator } from './log'
+
+describe('LogController Decorator', () => {
+  test('Should call controller handle', async () => {
+    class ControllerStub implements Controller {
+      // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+      handle(httpRequest: httpRequest): Promise<httpResponse> {
+        const httpResponse: httpResponse = {
+          statusCode: 200,
+          body: {
+            name: 'Luan'
+          }
+        }
+        return new Promise((resolve) => resolve(httpResponse))
+      }
+    }
+    const controllerStub = new ControllerStub()
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
+    const sut = new LogControllerDecorator(controllerStub)
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    await sut.handle(httpRequest)
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+})
