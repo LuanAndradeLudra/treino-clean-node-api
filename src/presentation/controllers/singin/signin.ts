37bc4b48d6899a5ef2a02ee697c15d2674909871
Luan Andrade <luanandradeti10@gmail.com>
2024-07-04 19:35:18 -0300

    feat: ensure LoginController calls EmailValidator with correct email

--- src/presentation/controllers/singin/signin.ts
+++ src/presentation/controllers/singin/signin.ts
@@ -1,8 +1,15 @@
 import { MissingParamError } from '../../errors'
 import { badRequest, ok } from '../../helpers/http-helper'
 import { IController, IHttpRequest, IHttpResponse } from '../../protocols'
+import { IEmailValidator } from '../singup/signup-protocols'
 
 export class SignInController implements IController {
+  private readonly emailValidator: IEmailValidator
+
+  constructor(emailValidator: IEmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   // eslint-disable-next-line require-await
   async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
     const requiredFields = ['email', 'password']
@@ -11,6 +18,8 @@ export class SignInController implements IController {
         return badRequest(new MissingParamError(field))
       }
     }
+
+    this.emailValidator.isValid(httpRequest.body['email'])
 
     return Promise.resolve(ok())
   }
