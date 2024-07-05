b1f36b49ce011d6b6807ae939f514d21c5760d04
Luan Andrade <luanandradeti10@gmail.com>
2024-07-05 17:31:26 -0300

    refactor: change (validator) name to (validation)

--- src/presentation/controllers/singup/singup.ts
+++ src/presentation/controllers/singup/singup.ts
@@ -3,17 +3,17 @@ import { IController, IHttpRequest, IHttpResponse, IAddAccount, IValidation } fr
 
 export class SignUpController implements IController {
   private readonly addAccount: IAddAccount
-  private readonly validator: IValidation
+  private readonly validation: IValidation
 
-  constructor(addAccount: IAddAccount, validator: IValidation) {
+  constructor(addAccount: IAddAccount, validation: IValidation) {
     this.addAccount = addAccount
-    this.validator = validator
+    this.validation = validation
   }
 
   async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
     try {
-      const validatorError = this.validator.validate(httpRequest.body)
-      if (validatorError) return badRequest(validatorError)
+      const validationError = this.validation.validate(httpRequest.body)
+      if (validationError) return badRequest(validationError)
 
       const account = await this.addAccount.add({
         name: httpRequest.body!['name'],
