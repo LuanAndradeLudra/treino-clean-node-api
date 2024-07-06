9ccfdb4a8772188f0fa05ee1da9aa84ba0e82d82
Luan Andrade <luanandradeti10@gmail.com>
2024-07-05 22:43:36 -0300

    refactor: remove unused code

--- src/presentation/controllers/singup/signup-protocols.ts
+++ src/presentation/controllers/singup/signup-protocols.ts
@@ -1,7 +1,6 @@
 /* istanbul ignore file */
 
 export * from '../../protocols'
-export * from '../../protocols/email-validator'
 export * from '../../../domain/usecases/add-account'
 export * from '../../../domain/models/account'
 export * from '../../../domain/models/add-account'
