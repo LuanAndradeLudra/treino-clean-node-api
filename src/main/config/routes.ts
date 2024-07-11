93aded36ef324f66e8099dd309b56b63a736d4b6
Luan Andrade <luanandradeti10@gmail.com>
2024-07-11 18:29:09 -0300

    refactor: change routes to use filesystem intead of fast-glob

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,9 +1,15 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import { Express, Router } from 'express'
-import fg from 'fast-glob'
+import { readdirSync } from 'fs'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  // eslint-disable-next-line @typescript-eslint/no-floating-promises
-  fg.sync('**/src/main/routes/**/**routes.ts').map(async (file) => (await import(`../../../${file}`)).default(router))
+  readdirSync(`${__dirname}/../routes`).map((folder) => {
+    readdirSync(`${__dirname}/../routes/${folder}`).map(async (file) => {
+      if (!file.includes('.test.')) {
+        (await import(`../routes/${folder}/${file}`)).default(router)
+      }
+    })
+  })
 }
